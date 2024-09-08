c7746b92ee49c0340df4141106c108286a699396
Aaron Loh <133678412+aaronloh16@users.noreply.github.com>
2024-09-08 01:01:20 -0400

    added search by author, title, registration, login, reviews, deleting reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,16 +31,42 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  const booksByAuthor = [];
+
+  Object.keys(books).forEach((isbn) => {
+    if (books[isbn].author === author) {
+      booksByAuthor.push(books[isbn]);
+    }
+  });
+
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor);
+  } else {
+    return res.status(404).json({ message: "No books found for this author" });
+  }
 });
 
+
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title.toLowerCase();
+  const booksByTitle = [];
+
+  Object.keys(books).forEach((isbn) => {
+    if (books[isbn].title.toLowerCase().includes(title)) {
+      booksByTitle.push(books[isbn]);
+    }
+  });
+
+  if (booksByTitle.length > 0) {
+    return res.status(200).json(booksByTitle);
+  } else {
+    return res.status(404).json({ message: "No books found with this title" });
+  }
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
